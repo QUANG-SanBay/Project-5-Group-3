034bd11faa81f66dfcf3926f32d0adb3616e53c0
QUANG-SanBay <quangsanbay@gmail.com>
2025-01-05 16:52:17 +0700

    thêm giao diện design

--- GiaCongTrangSuc/design/urls.py
+++ GiaCongTrangSuc/design/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from design import views
 
 urlpatterns = [
-    path('',views.get_home),
+    path('',views.get_home,name = 'home_design'),
+    path('thietke3d',views.get_tiepnhanthietke, name = 'tiepnhan_design'),
 ]
--- GiaCongTrangSuc/design/views.py
+++ GiaCongTrangSuc/design/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def get_home(request):
     return render(request,'design/home.html')
+def get_tiepnhanthietke(request):
+    return render(request,'design/tiepnhanthietke.html')
