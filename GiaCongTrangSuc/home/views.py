1ae1003536079d5c90245cd10196bab00e296a97
KhoaDangNguyen123 <dkhoa1112204@gmail.com>
2025-01-14 13:57:56 +0700

    Thêm vòng lặp sản phẩm lần 1

--- GiaCongTrangSuc/home/views.py
+++ GiaCongTrangSuc/home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from .models import *
+from .models import Product
 
 # Create your views here.
 def get_home(request):
-    return render(request,'home/home.html',{'timestamp': datetime.now().timestamp()})
+    products = Product.objects.all()
+    context={'products':products}
+    return render(request, 'home/home.html', {**context, 'timestamp': datetime.now().timestamp()})  
 def get_yeucaugiacong(request):
     return render(request,'home/datgiacong.html',{'timestamp': datetime.now().timestamp()})
