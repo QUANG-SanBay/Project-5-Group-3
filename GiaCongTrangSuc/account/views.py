6f966832947d31c8b7cb9274acf9ebcba6966611
qthang201105 <minhsang060802@gmail.com>
2025-01-18 13:32:26 +0700

    cập nhật thêm thông báo tạo tài khoản thành công

--- GiaCongTrangSuc/account/views.py
+++ GiaCongTrangSuc/account/views.py
@@ -9,18 +9,18 @@ def get_signUp(request):
         password = request.POST.get('password')
         password_confirmation = request.POST.get('password-confirmation')
 
-        # Tạo biến context để truyền thông báo lỗi cho riêng trang đăng ký
         context = {
             'form_name_error': None,
             'form_email_error': None,
             'form_password_error': None,
             'form_password_confirmation_error': None,
             'request': request,  # Truyền request để giữ giá trị các trường
+            'form_success': None,  # Thêm biến này để truyền thông báo thành công
         }
 
         # Kiểm tra Họ và Tên không được để trống
         if not full_name:
-            context['form_name_error'] = 'Họ và Tên không được để trống .'
+            context['form_name_error'] = 'Họ và Tên không được để trống.'
             return render(request, 'account/signUp.html', context)
 
         # Kiểm tra email đã tồn tại
@@ -47,15 +47,17 @@ def get_signUp(request):
         user.first_name = full_name  # Gán họ và tên
         user.save()
 
-        # Thông báo tạo tài khoản thành công
-        messages.success(request, 'Tài khoản đã được tạo thành công! Vui lòng đăng nhập.')
-        return redirect('login')  # Chuyển hướng tới trang đăng nhập
+        # Thêm thông báo thành công vào context
+        context['form_success'] = 'Tạo tài khoản thành công! Vui lòng đăng nhập.'
 
-    # GET request: Render form đăng ký trống
+        # Render lại trang đăng ký với thông báo thành công
+        return render(request, 'account/signUp.html', context)
+
+    # Trả về form đăng ký trống cho request GET
     return render(request, 'account/signUp.html')
 
 
 
+
 def get_login(request):
-    context = {}
-    return render(request,'account/login.html',context)
+    return render(request, 'account/login.html')
