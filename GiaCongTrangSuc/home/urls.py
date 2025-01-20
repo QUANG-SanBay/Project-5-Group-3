15e66e5f2b2f41f6dbcdc8363671030cbacab012
QUANG-SanBay <quangsanbay@gmail.com>
2025-01-20 14:59:27 +0700

    Thêm chức năng thông tin người dùng và 1 form thông tin người dùng
    chỉnh sửa các file: view,urls, headêr.html
    thêm file: infor-user.html, infor-user.js

--- GiaCongTrangSuc/home/urls.py
+++ GiaCongTrangSuc/home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.get_home,name='home'),
     path('datgiacong/',views.get_yeucaugiacong,name='datgiacong'),
+    path('inforUser/',views.get_inforUser,name='inforUser'),
 ]
