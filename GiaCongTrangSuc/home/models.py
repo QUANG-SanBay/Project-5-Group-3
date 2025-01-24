d534055e0435badc42c2bb8e41600f4821fc8fbc
QUANG-SanBay <quangsanbay@gmail.com>
2025-01-24 12:26:30 +0700

    chỉnh sửa lại database khách hàng
    xóa các class product_ring, product_collection,...
    def hàm tạo chi tiết sản phẩm khi tạo chỉnh sửa sản phẩm

--- GiaCongTrangSuc/home/models.py
+++ GiaCongTrangSuc/home/models.py
@@ -2,15 +2,25 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Customer(models.Model):
-    user = models.OneToOneField(User,on_delete=models.SET_NULL, null = True, blank=False)
-    name = models.CharField(max_length=200,null=True)
-    email = models.CharField(max_length=200,null=True)
+    SEX_LIST =[
+        ('nam', 'Nam'),
+        ('nu', 'Nữ'),
+        ('khac', 'Khác'),
+    ]
+    user = models.OneToOneField(User,on_delete=models.SET_NULL, null = True, blank=False,verbose_name='Người dùng')
+    address = models.CharField(max_length=200, null=True, blank=True, verbose_name='Địa chỉ')
+    sex = models.CharField(max_length=20, choices=SEX_LIST, null=True, blank=True, verbose_name='Giới tính')
+    phone = models.CharField(max_length=10, null=True, blank=True, verbose_name='Số điện thoại')
 
     def __str__(self):
-        return self.name
+        return self.user.first_name
+    #ham nay dung de chuyen huong den trang chi tiet khach hang
+    def get_absolute_url(self):
+        return reverse('customer_detail', args=[str(self.id)])
     
 
 class Product(models.Model):
@@ -26,70 +36,21 @@ class Product(models.Model):
         ('design_img', 'Design Image Product'),
         ('design_3d', 'Design 3D Product'),
     ]
-    name = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200, null=True,verbose_name='Tên sản phẩm')
     price = models.FloatField()
-    image_1 = models.ImageField(upload_to='product', null=True, blank=True)
-    image_2 = models.ImageField(upload_to='product', null=True, blank=True)
-    image_3 = models.ImageField(upload_to='product', null=True, blank=True)
-    image_4 = models.ImageField(upload_to='product', null=True, blank=True)
-    type = models.CharField(max_length=20, choices=PRODUCT_TYPES, null=True, blank=True)
-    design = models.CharField(max_length=20, choices=PRODUCT_DESIGN, null=True, blank=True)
+    image_1 = models.ImageField(upload_to='product', null=True, blank=True,verbose_name='Hình ảnh chính')
+    image_2 = models.ImageField(upload_to='product', null=True, blank=True, verbose_name='Hình ảnh 2')
+    image_3 = models.ImageField(upload_to='product', null=True, blank=True, verbose_name='Hình ảnh 3')
+    image_4 = models.ImageField(upload_to='product', null=True, blank=True, verbose_name='Hình ảnh 4')
+    type = models.CharField(max_length=20, choices=PRODUCT_TYPES, null=True, blank=True, verbose_name='Loại sản phẩm')
+    design = models.CharField(max_length=20, choices=PRODUCT_DESIGN, null=True, blank=True, verbose_name='Thiết kế')
 
     def __str__(self):
         return self.name
 
-class Product_Ring(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True) # Đổi thành product
-    def __str__(self):
-        return self.product.name 
-class Product_Earring(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True) # Đổi thành product
-    def __str__(self):
-        return self.product.name 
-class Product_Bracelet(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True) # Đổi thành product
-    def __str__(self):
-        return self.product.name 
-class Product_Pendant(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True) # Đổi thành product
-    def __str__(self):
-        return self.product.name 
-class Product_Gemstone(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True) # Đổi thành product
-    def __str__(self):
-        return self.product.name 
-class Product_Colletion(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True) # Đổi thành product
-    def __str__(self):
-        return self.product.name # hiển thị tên collection thay vì tên product
-class Product_DesginImg(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True) # Đổi thành product
-    def __str__(self):
-        return self.product.name 
-class Product_Desgin3D(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True) # Đổi thành product
-    def __str__(self):
-        return self.product.name 
+
 ## Tạo các bảng liên quan
-@receiver(post_save, sender=Product)
-def create_related_product(sender, instance, created, **kwargs):
-    if created:
-        if instance.type == 'ring':
-            Product_Colletion.objects.create(product=instance)
-        elif instance.type == 'earring':
-            Product_Earring.objects.create(product=instance)
-        elif instance.type == 'bracelet':
-            Product_Bracelet.objects.create(product=instance)
-        elif instance.type == 'pendant':
-            Product_Pendant.objects.create(product=instance)
-        elif instance.type == 'gemstone':
-            Product_Gemstone.objects.create(product=instance)
-        elif instance.type == 'collection':
-            Product_Colletion.objects.create(product=instance)
-        if instance.design == 'design_img':
-            Product_DesginImg.objects.create(product=instance)
-        elif instance.design == 'design_3d':
-            Product_Desgin3D.objects.create(product=instance)
+
 class Product_Details(models.Model):
     Details_material = [
         ('diamond', 'Diamond'),
@@ -123,36 +84,53 @@ class Product_Details(models.Model):
         ('24cm', '24cm'),
         ('25cm', '25cm'),
     ]
-    product = models.ForeignKey(Product,on_delete=models.SET_NULL, blank=True,null=True)
-    material = models.CharField(max_length=20, choices=Details_material, null=True, blank=True)
-    TrangSucDinhKem = models.CharField(max_length=20, choices=Details_TrangSucDinhKem, null=True, blank=True)
-    Daphu = models.CharField(max_length=20, choices=Details_Daphu, null=True, blank=True)
-    DaTam = models.CharField(max_length=20, choices=Details_DaTam, null=True, blank=True)
-    Size = models.CharField(max_length=20, choices=Details_Size, null=True, blank=True)
+    product = models.OneToOneField(Product,on_delete=models.SET_NULL, blank=True,null=True,verbose_name='Sản phẩm')
+    material = models.CharField(max_length=20, choices=Details_material, null=True, blank=True,verbose_name='Chất liệu')
+    TrangSucDinhKem = models.CharField(max_length=20, choices=Details_TrangSucDinhKem, null=True, blank=True,verbose_name='Trang sức đính kèm')
+    Daphu = models.CharField(max_length=20, choices=Details_Daphu, null=True, blank=True,verbose_name='Đá phụ')
+    DaTam = models.CharField(max_length=20, choices=Details_DaTam, null=True, blank=True,verbose_name='Đá tâm')
+    Size = models.CharField(max_length=20, choices=Details_Size, null=True, blank=True, verbose_name='Size')
     Describe = models.TextField(null=True, blank=True)
     def __str__(self):
         return str(self.product.name)
 class Oder(models.Model):
-    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL, blank=True,null=True)
-    product = models.ForeignKey(Product,on_delete=models.SET_NULL, blank=True,null=True)
-    date_oder = models.DateTimeField(auto_now_add=True)
-    compile = models.BooleanField(default=False,null=True,blank=False)
-    transaction_id = models.CharField(max_length=200, null=True)
+    STATUS_ORDER =[
+        ('DangXuLy', 'Đang xử lý'),
+        ('DaTiepNhan', 'Đã tiếp nhận'),
+        ('DangGiao', 'Đang giao'),
+        ('DaGiao', 'Đã giao'),
+    ]
+    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL, blank=True,null=True,verbose_name='Khách hàng')
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL, blank=True,null=True,verbose_name='Sản phẩm')
+    date_oder = models.DateTimeField(auto_now_add=True,verbose_name='Ngày đặt hàng')
+    status = models.CharField(max_length=20,choices=STATUS_ORDER, null=True, blank=True,verbose_name='Trạng thái')
+    quantity = models.IntegerField(default=0,null=True,blank=True,verbose_name='Số lượng')
     def __str__(self):
         return str(self.product.name)
-class OderItem(models.Model):
-    product = models.ForeignKey(Product,on_delete=models.SET_NULL, blank=True,null=True)
-    oder = models.ForeignKey(Oder,on_delete=models.SET_NULL, blank=True,null=True)
-    quantity = models.IntegerField(default=0,null=True,blank=True) #so luong
-    date_added = models.DateTimeField(auto_now_add=True)
-
-class ShoppingAddress(models.Model):
-    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL, blank=True,null=True)
-    oder = models.ForeignKey(Oder,on_delete=models.SET_NULL, blank=True,null=True)
-    address = models.CharField(max_length=200,null=True)
-    city = models.CharField(max_length=200,null=True)
-    state = models.CharField(max_length=200,null=True)
-    mobile = models.CharField(max_length=10,null=True)# tinh
-    date_added = models.DateTimeField(auto_now_add=True)
+    
+    
+class Blog(models.Model):
+    title = models.CharField(max_length=200, null=True,verbose_name='Tiêu đề')
+    content = models.TextField(null=True, blank=True, verbose_name='Nội dung')
+    image = models.ImageField(upload_to='blog', null=True, blank=True,verbose_name='Hình ảnh')
+    date = models.DateTimeField(auto_now_add=True,verbose_name='Ngày đăng')
     def __str__(self):
-        return str(self.address)
+        return str(self.title)
+@receiver(post_save, sender=Product)
+def create_or_update_product_details(sender, instance, created, **kwargs):
+    """
+    Hàm này được gọi mỗi khi một Product được tạo hoặc cập nhật.
+    Nó sẽ tạo hoặc cập nhật Product_Details tương ứng.
+    """
+    product_details, created_details = Product_Details.objects.get_or_create(product_id=instance.id) 
+
+# class ShoppingAddress(models.Model):
+#     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL, blank=True,null=True)
+#     oder = models.ForeignKey(Oder,on_delete=models.SET_NULL, blank=True,null=True)
+#     address = models.CharField(max_length=200,null=True)
+#     city = models.CharField(max_length=200,null=True)
+#     state = models.CharField(max_length=200,null=True)
+#     mobile = models.CharField(max_length=10,null=True)# tinh
+#     date_added = models.DateTimeField(auto_now_add=True)
+#     def __str__(self):
+#         return str(self.address)
