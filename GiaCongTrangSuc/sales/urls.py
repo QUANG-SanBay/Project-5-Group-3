40b3266c109bee3aa16c07ea3e2c840a485128be
Tran-Ngoc-Qui <tranngocqui.151020@gmail.com>
2025-02-27 13:07:07 +0700

    Revert "update"
    This reverts commit 0cb821fa52c149ac7006d0f01ecec75bc32b2ba1.

--- GiaCongTrangSuc/sales/urls.py
+++ GiaCongTrangSuc/sales/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_home, name='homeSales'),
-    path('yeucaugiacong/', views.get_tiepnhangiacong, name='yeucaugiacongSales'),
-    path('create_order/', views.create_order, name='create_order'),
+    path('',views.get_home,name='homeSales'),
+    path('yeucaugiacong/',views.get_tiepnhangiacong,name='yeucaugiacongSales'),
 ]
